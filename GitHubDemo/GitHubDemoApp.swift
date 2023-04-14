cd4fb5ff20295364b3a9de1abf1a76a7c7cbc673
Vlad <Vlad Bakh>
2023-04-14 19:57:29 +0300

    Initial Commit

---
+++ GitHubDemo/GitHubDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubDemoApp.swift
+//  GitHubDemo
+//
+//  Created by user on 14.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct GitHubDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
